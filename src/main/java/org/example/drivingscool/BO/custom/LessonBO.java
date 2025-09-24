a96ea062230c6087c147ca852b3def1d4ce9b4a8
Hashini Devindi <hashinidevindi558@gmail.com>
2025-09-24 11:59:09 +0530

    lesson

--- src/main/java/org/example/drivingscool/BO/custom/LessonBO.java
+++ src/main/java/org/example/drivingscool/BO/custom/LessonBO.java
@@ -1,6 +1,27 @@
 package org.example.drivingscool.BO.custom;
 
 import org.example.drivingscool.BO.SuperBO;
+import org.example.drivingscool.model.LessonDTO;
+import org.example.drivingscool.entity.Student;
+import org.example.drivingscool.entity.Course;
+import org.example.drivingscool.entity.Instructor;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public interface LessonBO extends SuperBO {
+    List<LessonDTO> getAllLessons() throws SQLException;
+
+    boolean save(LessonDTO dto) throws Exception;
+
+    boolean update(LessonDTO dto) throws SQLException, ClassNotFoundException;
+
+    boolean delete(String id) throws Exception;
+
+    // Methods to get data for ComboBoxes
+    List<Student> getAllStudents() throws SQLException;
+
+    List<Course> getAllCourses() throws SQLException;
+
+    List<Instructor> getAllInstructors() throws SQLException;
 }
--- src/main/java/org/example/drivingscool/BO/custom/impl/LessonBOImpl.java
+++ src/main/java/org/example/drivingscool/BO/custom/impl/LessonBOImpl.java
@@ -1,6 +1,47 @@
 package org.example.drivingscool.BO.custom.impl;
 
 import org.example.drivingscool.BO.custom.LessonBO;
+import org.example.drivingscool.entity.Course;
+import org.example.drivingscool.entity.Instructor;
+import org.example.drivingscool.entity.Student;
+import org.example.drivingscool.model.LessonDTO;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class LessonBOImpl implements LessonBO {
+    @Override
+    public List<LessonDTO> getAllLessons() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public boolean save(LessonDTO dto) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean update(LessonDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        return false;
+    }
+
+    @Override
+    public List<Student> getAllStudents() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public List<Course> getAllCourses() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public List<Instructor> getAllInstructors() throws SQLException {
+        return List.of();
+    }
 }
